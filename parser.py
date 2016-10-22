c01db2cd4f3e5e89e59ba239ade0e0ea42185fe4
Jim <jim.lemmers@gmail.com>
2016-10-22 13:30:48 +0200

    fixed old file commit

--- parser.py
+++ parser.py
@@ -1,15 +1,18 @@
 from lxml import etree
-from multiprocessing import Pool
+from multiprocessing import Process
+from multiprocessing import Queue
 import os
 import time
-from elasticsearch import Elasticsearch
-import pyelasticsearch
+from pyelasticsearch import ElasticSearch, utils
 
-es = Elasticsearch(hosts=['http://localhost:9200/'])
 
-def parsexml(xml):
+class Worker(Process):
+    def __init__(self, in_q, out_q):
+        super(Worker, self).__init__()
+        self.in_q = in_q
+        self.out_q = out_q
 
-    def get_fields(parsed):
+    def get_fields(self, parsed):
         if len(parsed) > 3:
             date = parsed[0]
             typ = parsed[1]
@@ -17,7 +20,7 @@ def parsexml(xml):
             title = ''
 
             if len(parsed) == 4:
-                text= parsed[3]
+                text = parsed[3]
             else:
                 title = parsed[3]
                 text = ' '.join(parsed[4:])
@@ -32,30 +35,63 @@ def parsexml(xml):
             return c_dict
         return {}
 
-    try:
-        tree = etree.iterparse(xml, events=('end',), tag='root')
-    except:
-        print(Exception)
-        print(xml, 'failed')
-        return []
-    t = time.time()
-    parsed = []
+    def run(self):
+        print('started')
 
-    for event, elem in tree:
-        parsed.append(get_fields(list(elem.itertext())))
-        elem.clear()
+        while True:
+            xml = self.in_q.get()
+            if xml is None:
+                break
 
-    del tree
-    return parsed
-    print('parsing took', time.time() -t )
-    print('writing to disk')
-    # RENS HIER KOMT JE CODE JONGE!
-    es.bulk(parsed)
-'''
-p = Pool(4)
-folder = '../Telegraaf/telegraaf/'
+            t = time.time()
+            tree = etree.iterparse(xml, events=('end',), tag='root')
+            parsed = []
+            for event, elem in tree:
+                parsed.append(get_fields(list(elem.itertext())))
+                elem.clear()
+            print('doing', xml)
+            self.out_q.put(parsed)
+            print('did', xml, 'took', time.time() - t)
+
+
+class StoreWorker(Process):
+    def __init__(self, out_q):
+        super(StoreWorker, self).__init__()
+        self.out_q = out_q
+
+    def run(self):
+        while True:
+            articles = self.out_q.get()
+            if articles is None:
+                break
+            for chunk in utils.bulk_chunks(articles, docs_per_chunk=200):
+                es.bulk(chunk, index='telegraaf', doc_type='artikel')
+            del articles
+
+es = ElasticSearch('http://localhost:9200')
+try:
+    es.delete_index('telegraaf')
+except:
+    print('index did not exist')
+
+folder = '/home/jim/data/'
 t = time.time()
 files = [os.path.abspath(folder+f) for f in os.listdir(os.path.abspath(folder))
-             if f[-3:] == 'xml']
-p.map(parsexml, files)
-print(time.time() - t)'''
+         if f[-3:] == 'xml']
+
+in_q = Queue()
+out_q = Queue()
+workers = [Worker(in_q, out_q) for _ in range(2)]
+storer = StoreWorker(out_q)
+
+for f in sorted(files):
+    in_q.put(f)
+
+for i in range(2):
+    workers[i].start()
+
+storer.start()
+print('Going for it!')
+
+storer.join()
+print(time.time() - t)
