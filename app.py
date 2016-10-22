e37564c0e78abc75413aa0d60bb96396128ab54c
Rens <rens.mester@gmail.com>
2016-10-22 17:18:44 +0200

    search query
    be sure to check if names in the filter part are correct

--- app.py
+++ app.py
@@ -10,8 +10,38 @@ def index():
 
 @app.route('/api/suggest', methods=['POST'])
 def suggest():
-    # hier komt code die zoekt met behulp van elasticsearch
-    # en de json die er uit angular komt.
+
+@app.rout('/api/search', methods=['POST'])
+def search():
+    searchterm = request.form.get('query', '')
+    startdate = request.form.get('startdate', '1918')
+    enddate = request.form.get('enddate', '1994')
+    title = request.form.get('title', '')
+
+    query = {"query": {
+                "filtered": {
+                    "query": {
+                        # match the following input in the fiels
+                        "multi_match": {
+                            "query": , searchterm
+                            "fields": ["text", "title"]
+                            }
+                    },
+                    "filter" :{
+                        # filter on date range gte >=, lte <=
+                        "range": {
+                            #!!! not sure if name is correct
+                            "date": {
+                                "gte": startdate,
+                                "lte": enddate
+                                }
+                            },
+                        #!!! not sure, filter on the title of term
+                        'term' : {'title': title}
+                        }
+                    }
+                }
+            }
     pass
 
 if __name__ == '__main__':
