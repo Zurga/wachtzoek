472656d2444141714388f9eb40da65d8d22644d4
Jim <jim.lemmers@gmail.com>
2016-10-22 12:04:33 +0200

    tried to make the parser faster by making it multiprocessing

--- parser.py
+++ parser.py
@@ -1,13 +1,18 @@
 from lxml import etree
-from multiprocessing import Pool
+from multiprocessing import Process
+from multiprocessing import Queue
 import os
 import time
+from pyelasticsearch import ElasticSearch, utils
 
 
-def parsexml(xml):
-    parser = etree.XMLParser(huge_tree=True)
+class Worker(Process):
+    def __init__(self, in_q, out_q):
+        super(Worker, self).__init__()
+        self.in_q = in_q
+        self.out_q = out_q
 
-    def get_fields(parsed):
+    def get_fields(self, parsed):
         if len(parsed) > 3:
             date = parsed[0]
             typ = parsed[1]
@@ -15,7 +20,7 @@ def parsexml(xml):
             title = ''
 
             if len(parsed) == 4:
-                text= parsed[3]
+                text = parsed[3]
             else:
                 title = parsed[3]
                 text = ' '.join(parsed[4:])
@@ -30,24 +35,64 @@ def parsexml(xml):
             return c_dict
         return {}
 
-    try:
-        tree = etree.parse(xml, parser=parser)
-    except:
-        print(Exception)
-        print(xml, 'failed')
-        return []
-    print('opened', xml)
-    t = time.time()
-    parsed = (get_fields(text) for child in tree.getroot().iterchildren()
-              for text in child.itertext())
-    print('parsing took', time.time() -t )
-    print('writing to disk')
-    # RENS HIER KOMT JE CODE JONGE!
+    def run(self):
+        print('started')
 
-p = Pool(4)
+        while True:
+            xml = self.in_q.get()
+            if xml is None:
+                break
+
+            parser = etree.XMLParser(huge_tree=True)
+
+            print('doing', xml)
+            t = time.time()
+            tree = etree.parse(xml, parser=parser)
+            self.out_q.put([es.index_op(self.get_fields(text))
+                            for child in tree.getroot().iterchildren()
+                            for text in child.itertext()])
+            del tree
+            print('did', xml, 'took', time.time() - t)
+
+
+class StoreWorker(Process):
+    def __init__(self, out_q):
+        super(StoreWorker, self).__init__()
+        self.out_q = out_q
+
+    def run(self):
+        while True:
+            articles = self.out_q.get()
+            if articles is None:
+                break
+            for chunk in utils.bulk_chunks(articles, docs_per_chunk=200):
+                es.bulk(chunk, index='telegraaf', doc_type='artikel')
+            del articles
+
+es = ElasticSearch('http://localhost:9200')
+try:
+    es.delete_index('telegraaf')
+except:
+    print('index did not exist')
+
 folder = '/home/jim/data/'
 t = time.time()
 files = [os.path.abspath(folder+f) for f in os.listdir(os.path.abspath(folder))
-             if f[-3:] == 'xml']
-p.map(parsexml, files)
+         if f[-3:] == 'xml']
+
+in_q = Queue()
+out_q = Queue()
+workers = [Worker(in_q, out_q) for _ in range(2)]
+storer = StoreWorker(out_q)
+
+for f in sorted(files):
+    in_q.put(f)
+
+for i in range(2):
+    workers[i].start()
+
+storer.start()
+print('Going for it!')
+
+storer.join()
 print(time.time() - t)
