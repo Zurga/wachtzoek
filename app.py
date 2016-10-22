9894d7b66bd09e5cd9a25fee3daa04453072938e
nibnob <email@davidchilders.nl>
2016-10-22 17:44:39 +0200

    Merge errors

--- app.py
+++ app.py
@@ -75,6 +75,38 @@ def suggest():
     return render_template('result.html',
                            data=data)
 
+@app.rout('/api/search', methods=['POST'])
+def search():
+    searchterm = request.form.get('query', '')
+    startdate = request.form.get('startdate', '1918')
+    enddate = request.form.get('enddate', '1994')
+    title = request.form.get('title', '')
+
+    query = {"query": {
+                "filtered": {
+                    "query": {
+                        # match the following input in the fiels
+                        "multi_match": {
+                            "query": , searchterm
+                            "fields": ["text", "title"]
+                            }
+                    },
+                    "filter" :{
+                        # filter on date range gte >=, lte <=
+                        "range": {
+                            #!!! not sure if name is correct
+                            "date": {
+                                "gte": startdate,
+                                "lte": enddate
+                                }
+                            },
+                        #!!! not sure, filter on the title of term
+                        'term' : {'title': title}
+                        }
+                    }
+                }
+            }
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
