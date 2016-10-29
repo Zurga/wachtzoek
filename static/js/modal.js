2c29a267f0a27ad0284294120133bb5fca8c966f
Jim <jim.lemmers@gmail.com>
2016-10-29 15:42:08 +0200

    fixed the scoring js

--- static/js/modal.js
+++ static/js/modal.js
@@ -12,3 +12,15 @@ function delModal(identifier) {
     $( ".modal-container" ).hide();
     $( ".modal-container" ).empty();
 }
+
+// creates a query to the database with the relevancy
+function score() {
+    var request = {'relevant': this.value,
+                   'query': $('#query_string')[0].value,
+                   //'judge': $('#judge')[0].value,
+                   'doc_id': $('#modal-overall #doc_id')[0].value,
+                   };
+    $.post('/api/score', request, function (data) {
+        $(this).css('background-color', '#00ee00');
+    });
+};
